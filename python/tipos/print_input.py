23db9aa082c710fe0c30136f48cdf867ed8751f4
ftaveira-dev <ftaveira.dev@gmail.com>
2024-09-22 19:20:52 -0300

    estudos de input e print

---
+++ python/tipos/print_input.py
@@ -0,0 +1,7 @@
+nome = input("informe seu nome: ")
+idade = input("Informe sua idade: ")
+
+print(f"Seu nome é {nome}")
+print(nome, idade, end="...\n")
+print(nome, idade, sep="#")
+print(nome, idade)
