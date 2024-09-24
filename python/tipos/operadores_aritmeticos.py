8eed44daf64d382be6a34a9b66f432cc0d409de9
ftaveira-dev <ftaveira.dev@gmail.com>
2024-09-24 19:11:27 -0300

    aula sobre operadores aritmeticos

---
+++ python/tipos/operadores_aritmeticos.py
@@ -0,0 +1,9 @@
+produto_1 = 35
+produto_2 = 25
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 * produto_2)
+print(produto_1 / produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
