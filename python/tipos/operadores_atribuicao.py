8e93b9092dfe3fbebff44f47541ad8af4e879c02
ftaveira-dev <ftaveira.dev@gmail.com>
2024-09-24 19:28:18 -0300

    aula sobre opoeradores de atribuicao

---
+++ python/tipos/operadores_atribuicao.py
@@ -0,0 +1,21 @@
+saldo = 500
+print(saldo)
+
+saldo = 200
+print(saldo)
+
+saldo +=500
+print(saldo)
+
+saldo -= 500
+print(saldo)
+
+saldo /= 500
+print(saldo)
+
+saldo *= 11900
+print(saldo)
+
+saldo //= 500
+print(saldo)
+
