893345cd4c6f138435a8e279f4ee37bfe73da9bd
ftaveira-dev <ftaveira.dev@gmail.com>
2024-09-29 15:20:32 -0300

    auala sobre funcoes comuns

---
+++ python/funcoes/funcoes_comum.py
@@ -0,0 +1,51 @@
+#Exemplo de funcao comum
+
+def exibir_mensagem():
+    print("Olá Mundo!")
+
+def exibir_mensagem_2(nome):
+    print(f"Seja bem vindo {nome}!")
+
+def exibir_mensagem_3(nome= "Chico"):
+    print(f"Seja bem vindo {nome}")
+
+
+exibir_mensagem()
+exibir_mensagem_2(nome="Francisco") #preciso declarar a variavel em algum momento.
+exibir_mensagem_3()
+exibir_mensagem_3(nome="kid")
+
+
+#Exemplo de funcao com return 
+
+def calcular_total(numeros):
+    return sum(numeros)
+
+def retorna_antecessor_e_sucessor(numero):
+    antecessor = numero -1 
+    sucessor = numero + 1
+
+    return antecessor, sucessor
+
+print(calcular_total([10,20,34])) #64
+print(retorna_antecessor_e_sucessor(10)) #9 e 11 *args=valores vem em tupl
+
+#Exemplo com argumentos nomeados
+
+def salvar_carro(marca, modelo, ano, placa):
+    print(f"Carro inserido com sucesso! {marca}/{modelo}/{ano}/{placa}")
+
+salvar_carro("Fiat", "Palio", 1999, "ABC-1243")
+salvar_carro(marca="Fiat", modelo="Palio", ano=1999, placa="ABC-1243")
+salvar_carro(**{"marca": "Fiat", "modelo": "Palio", "ano": 1999, "placa": "ABC-1243"}) #**kwargs=valores vem em dicionario
+
+#Exemplo com *args=valores vem em tupla e **kwargs=valores vem em dicionario
+
+def exibir_poema(data_extenso, *args, **kwargs):
+    
+
+
+
+
+
+
